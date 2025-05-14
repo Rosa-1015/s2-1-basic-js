b6d41eddfb86273dda155f759c2eb70a3cde12bf
Rosa Martín <rosa.ms@gmail.com>
2025-05-14 21:53:43 +0200

    Exercise 1_1 Arrow functions

---
+++ exercise5/script.js
@@ -0,0 +1,8 @@
+function passarTemps(){
+    const mostrarMissatge = () => {
+        setTimeout(() => {
+        console.log("Hola! Han passat 3 segons.");
+        }, 3000);
+    };
+    mostrarMissatge();
+}
