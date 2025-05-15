1b32e9313898ca8a2cee5bd0a26999c9571dea66
Rosa Martín <rosa.ms@gmail.com>
2025-05-15 20:05:41 +0200

    Mandatory exercises done

--- exrecise-1-1-4/script.js
+++
@@ -1,8 +0,0 @@
-const numeros = [1, 2, 3, 4, 5];
-
-function printNumbers() {
-    for (let i = 0; i < numeros.length; i++) {
-        const imprimir = () => console.log(numeros[i]);
-        imprimir();
-    }
-}
